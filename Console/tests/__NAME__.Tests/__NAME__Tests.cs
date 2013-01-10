8ebb2426c4ceb8f775bfc4a879cbdc5c9bd6c78d
crazykarl <haffeyr@gmail.com>
2013-01-10 16:28:17 -0500

    Adding Console Template

---
+++ Console/tests/__NAME__.Tests/__NAME__Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace __NAME__.Tests
+{
+    [TestFixture]
+    public class __NAME__Tests {
+        
+        [TestFixtureSetUp]
+        public void FixtureSetup() {
+            
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTeardown() {
+            
+        }
+
+        [SetUp]
+        public void TestSetup() {
+            
+        }
+
+        [TearDown]
+        public void TestTeardown() {
+            
+        }
+    }
+}
